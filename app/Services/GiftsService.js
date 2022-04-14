b0c0f5345a2856177748415f6da95a09ccd9e159
AndrewLaRue <96216815+AndrewLaRue@users.noreply.github.com>
2022-04-14 16:37:42 -0600

    init

--- app/Services/GiftsService.js
+++ app/Services/GiftsService.js
@@ -3,23 +3,23 @@ import { Gift } from "../Models/Gift.js"
 import { gifApi } from "./AxiosService.js"
 
 
-class GiftsService{
-    async getGifts(){
+class GiftsService {
+    async getGifts() {
         let params = {
-            api_key:'Cuyr7covC5ZGg38MJ4huELUmAh6jVZs0',
+            api_key: 'Cuyr7covC5ZGg38MJ4huELUmAh6jVZs0',
             rating: 'pg',
             q: 'random',
-            limit: '1'
+            limit: '3'
         }
-     
-        const res = await gifApi.get('', {params})
+
+        const res = await gifApi.get('', { params })
         const gift = res.data.data.map(g => new Gift(g))
-        console.log("the res",res.data);
+        console.log("the res", res.data);
         ProxyState.gift = gift
         console.log("proxystate", ProxyState.gift)
-        
+
     }
-  
+
 }
 
 export const giftsService = new GiftsService()
